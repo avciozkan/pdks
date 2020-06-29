f3bf1ba6e69c8cc627a7050404d21b69b77113ab
avciozkan <ozkanavci23@gmail.com>
2020-06-29 23:50:16 +0300

    Annotation Eksikligi

--- src/main/java/com/inomera/pdks/PdksApplication.java
+++ src/main/java/com/inomera/pdks/PdksApplication.java
@@ -2,9 +2,18 @@ package com.inomera.pdks;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ComponentScans;
+
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
 
 @SpringBootApplication
+@EnableEncryptableProperties
+@ComponentScans({ @ComponentScan(basePackages = "com.inomera.pdks.rest") })
 public class PdksApplication {
+	static {
+		System.setProperty("jasypt.encryptor.password", "SimplePassword");
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(PdksApplication.class, args);
